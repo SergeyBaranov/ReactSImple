074a0f087af626dee7f2107888c641f2ed58c59e
Sergey Baranov <workinggrid@gmail.com>
2025-09-22 19:27:50 +0300

    added project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Header from './components/Header.jsx'
+
+export default function App() {
+  return (
+    
+    <div>
+      <Header />
+      <h1>Hello React!</h1>
+    </div>
+  )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header>
+      <h3>Test case for React App</h3>
+      <span>Time place</span>
+    </header>
+  )
+}
